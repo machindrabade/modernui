14f14e95ad396844074730593f999d9d8eb01b86
MachindraRajendraBad <machindrabade9595@gmail.com>
2024-11-25 08:23:23 +0530

    modern ui

---
+++ lib/pages/mainpage.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:modernlogintute/pages/login_page.dart';
+
+import 'HomePage.dart';
+
+class mainpage extends StatelessWidget {
+  const mainpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context,snapshot) {
+    if(snapshot.hasData) {
+    return HomePage();
+    }else{
+     return LoginPage();
+    }
+      },
+      ),
+    );
+  }
+}
